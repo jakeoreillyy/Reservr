07470c25a526d21a9e938029d6ca35ebb606a4e0
jakeoreilly <C24321986@mytudublin.ie>
2025-11-10 20:39:15 +0000

    Fixing Database connection code

--- includes/database_connection.php
+++ includes/database_connection.php
@@ -4,15 +4,9 @@ $username = "root";
 $password = "";
 $dbname = "reservr";
 
-$error_message = "";
-$success_message = "";
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $conn = new mysqli($servername, $username, $password, $dbname);
 
-
   if ($conn->connect_error) {
-    $error_message = "Connection failed: " . $conn->connect_error;
-    }
-}
+    die("Connection failed: " . $conn->connect_error);
+  }
 ?>
--- pages/registration.php
+++ pages/registration.php
@@ -1,64 +1,68 @@
 <?php
 include '../includes/database_connection.php';
 
-$title = $_POST['title'];
-$first_name = $_POST['first_name'];
-$surname = $_POST['surname'];  
-$email = $_POST['email'];
-$phone = $_POST['phone'];
-$address = $_POST['address'];
-$city = $_POST['city'];
-$country = $_POST['country'];
-$password = $_POST['password'];
-$confirm_password = $_POST['confirm_password'];
-
+$error_message = "";
+$success_message = "";
 $validate_form = true;
 $errors = [];
 
-if (strlen($password) != 6) {
-  $errors[] = "Password must be 6 digits";
-  $validate_form = false;
-} else {
-  if ($password !== $confirm_password) {
-    $errors[] = "Passwords do not match";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $title = $_POST['title'];
+  $first_name = $_POST['first_name'];
+  $surname = $_POST['surname'];  
+  $email = $_POST['email'];
+  $phone = $_POST['phone'];
+  $address = $_POST['address'];
+  $city = $_POST['city'];
+  $country = $_POST['country'];
+  $password = $_POST['password'];
+  $confirm_password = $_POST['confirm_password'];
+
+  if (strlen($password) != 6) {
+    $errors[] = "Password must be 6 digits";
+    $validate_form = false;
+  } else {
+    if ($password !== $confirm_password) {
+      $errors[] = "Passwords do not match";
+      $validate_form = false;
+    }
+  }
+
+  $phone = preg_replace('/\D/', '', $phone);
+
+  if (strlen($phone) != 10) {
+    $errors[] = "Phone number must be 10 digits";
     $validate_form = false;
   }
-}
 
-$phone = preg_replace('/\D/', '', $phone);
-
-if (strlen($phone) != 10) {
-  $errors[] = "Phone number must be 10 digits";
-  $validate_form = false;
-}
-
-if (count($errors) > 0) {
-  $error_message = implode("<br>", $errors);
-}
-
-if ($validate_form) {
-  $password_hash = password_hash($password, PASSWORD_DEFAULT);
-
-  $stmt = $conn->prepare("INSERT INTO users (title, first_name, surname, email, phone, address, city, country, password_hash) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
-  $stmt->bind_param("sssssssss", $title, $first_name, $surname, $email, $phone, $address, $city, $country, $password_hash);
-
-  try {
-    if ($stmt->execute()) {
-      $success_message = "Account created successfully! You can now login <a href='../index.php'>here</a>";
-    }
-  } catch (mysqli_sql_exception $e) {
-    if (strpos($e->getMessage(), 'email_unique') !== false) {
-        $error_message = "This email is already registered. <a href='../index.php'>Login instead?</a>";
-    } elseif (strpos($e->getMessage(), 'phone_unique') !== false) {
-        $error_message = "This phone number is already registered.";
-    } else {
-        $error_message = "Registration failed. Please try again.";
-    }
+  if (count($errors) > 0) {
+    $error_message = implode("<br>", $errors);
   }
-    
-  $stmt->close();
+
+  if ($validate_form) {
+    $password_hash = password_hash($password, PASSWORD_DEFAULT);
+
+    $stmt = $conn->prepare("INSERT INTO users (title, first_name, surname, email, phone, address, city, country, password_hash) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
+    $stmt->bind_param("sssssssss", $title, $first_name, $surname, $email, $phone, $address, $city, $country, $password_hash);
+
+    try {
+      if ($stmt->execute()) {
+        $success_message = "Account created successfully! You can now login <a href='../index.php'>here</a>";
+      }
+    } catch (mysqli_sql_exception $e) {
+      if (strpos($e->getMessage(), 'email') !== false) {
+          $error_message = "This email is already registered. <a href='../index.php'>Login instead?</a>";
+      } elseif (strpos($e->getMessage(), 'phone') !== false) {
+          $error_message = "This phone number is already registered.";
+      } else {
+          $error_message = "Registration failed. Please try again.";
+      }
+    }
+      
+    $stmt->close();
+  }
+  $conn->close();
 }
-$conn->close();
 ?>
 
 <!DOCTYPE html>
