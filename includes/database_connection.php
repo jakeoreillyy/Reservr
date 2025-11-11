8011d6c0f2c691426a68c3c444251f96f845dbd2
jakeoreilly <C24321986@mytudublin.ie>
2025-11-11 00:08:33 +0000

    Adding dashboard.php

--- includes/database_connection.php
+++ includes/database_connection.php
@@ -4,9 +4,9 @@ $username = "root";
 $password = "";
 $dbname = "reservr";
 
-  $conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-  if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-  }
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
 ?>
---
+++ pages/dashboard.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
+    <title>Reservr | Dashborad</title>
+</head>
+<body>
+    <nav>
+      <ul>
+        <li class="logo">
+          <a href="dashboard.php">
+            <img src="../src/logo.svg" alt="Library icon">
+          </a>
+        </li>  
+        <li>
+          <a href="dashboard.php">View</a>
+        </li>
+        <li>
+          <a href="dashboard.php" class="active">Search</a>
+        </li>
+      </ul>
+    </nav>  
+</body>
+</html>
